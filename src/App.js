f3d3fbbcfd4c77683e41ebcbba1e1c4325c4eb09
Sergio <sdosoriogonzalez19@outlook.es>
2023-12-20 13:56:13 -0500

    bases of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container-freecodecamp-logo'>
+        <img
+          className='freecodecamp-logo'
+          src={logo}
+          alt='Freecodecamp logo'
+        />
+      </div>
+      <div className='main-container'>
+        
+      </div>
     </div>
   );
 }
